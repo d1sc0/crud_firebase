903bba75b641e3b26a4945f5793766fc999fde94
Stuart Mackenzie <ilovediscostu@gmail.com>
2021-03-29 16:37:02 +0100

    update cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,31 @@
 const functions = require("firebase-functions");
+const Vonage = require('@vonage/server-sdk')
 const config = functions.config();
 const vonageKey = config.env.vonage.apiKey
 const vonageSecret = config.env.vonage.apiSecret
-
+const vonage = new Vonage({
+  apiKey: vonageKey,
+  apiSecret: vonageSecret
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.sendSMS = functions.https.onRequest((request, response) => {
+let from = "Send Pic Test"
+let to = "447804521377"
+let text = 'A text message sent using the Vonage SMS API https://google.com'
+vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+        functions.logger.info(err, {structuredData: true});
+    } else {
+        if(responseData.messages[0]['status'] === "0") {
+            functions.logger.info("Message sent successfully", {structuredData: true});
+            response.send("Message sent successfully!");
+        } else {
+            functions.logger.info(`Message failed with error: ${responseData.messages[0]['error-text']}`, {structuredData: true});
+            response.send(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+        }
+    }
+})
+
+});
