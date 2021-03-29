5f7d910346ca1283f55822201dd28ac218b63d49
Stuart Mackenzie <ilovediscostu@gmail.com>
2021-03-29 20:08:58 +0100

    update

--- public/scripts/sendPic.js
+++ public/scripts/sendPic.js
@@ -3,7 +3,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 const id = urlParams.get('id')
 var user = auth.currentUser
-var cards = ""
+var imageTitle, imageUrl, cards
 
 auth.onAuthStateChanged(_user => {
 user = _user
@@ -12,26 +12,28 @@ user = _user
 console.log(id);
 
 db.collection('photos').doc(id).get().then((card)=>{
-  renderCard(card)
+  imageTitle = card.data().imageTitle
+  imageUrl = card.data().imageUrl
+  renderCard(imageTitle, imageUrl)
  })
 
-function renderCard(card) {
+function renderCard(imageTitle, imageUrl) {
 //replace title
   var deleteTitle = document.querySelector('.del-title')
-  deleteTitle.textContent = card.data().imageTitle
+  deleteTitle.textContent = imageTitle
 //replace image link
  var figure = document.querySelector('.image')
  var imgNode = document.createElement('img')
  figure.appendChild(imgNode)
- figure.childNodes[1].setAttribute('src', card.data().imageUrl)
+ figure.childNodes[1].setAttribute('src', imageUrl)
  //replace alt
- figure.childNodes[1].setAttribute('alt', card.data().imageTitle) 
+ figure.childNodes[1].setAttribute('alt', imageTitle) 
 
 if (!user) {
   var controls = document.querySelector('.controls')
   controls.innerHTML = `<a href="login.html" class="button is-danger login-btn">You must be signed into send!</a>`
 }
-     
+
 }
 
 sendForm.addEventListener('submit', (e)=> {
@@ -45,12 +47,12 @@ fetch('https://us-central1-crud-firebase-c1897.cloudfunctions.net/sendSMS', {
   },
   body: JSON.stringify({
         sms: {
-            title: "lovely hands",
+            title: imageTitle,
             recipient: "447804521377",
-            link: "https://firebasestorage.googleapis.com/v0/b/crud-firebase-c1897.appspot.com/o/photos%2FivBfPa5j9edtsFMCvxxLSLBGABK2%2Fhands.jpg?alt=media&token=5c8db61c-0d21-4243-9f41-71e5b6e671cd"
+            link: imageUrl
         }
     })
 }).then(res => {
-  console.log("response:", res);
+  console.log(res);
 });
 })
